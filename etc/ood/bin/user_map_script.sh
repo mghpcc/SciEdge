549c8bbc8b384604bbcd726faa659fea4a11ceb9
Morgan Ludwig <mjbludwig@gmail.com>
2022-06-23 13:39:55 -0400

    add basic ldapsearch functionality to user_map script

--- etc/ood/bin/user_map_script.sh
+++ etc/ood/bin/user_map_script.sh
@@ -6,10 +6,18 @@ USERNAME=$(echo $INPUT | awk -F@ '{print $1}')
 if [ $? -eq 0  ]; then
 
 	#Can check ldap here if ldap is configured
-	# LDAPUSER=$(ldapsearch -x uid=$USERNAME | grep "uid:" | awk '{print $2}') 
+	LDAPUSER=$(ldapsearch -x uid=$USERNAME | grep "uid:" | awk '{print $2}') 
+	LOCALUSER=$(getent passwd $USERNAME)
+
+	if [[ "$LDAPUSER" == "$LOCALUSER" ]]; then
+		echo $LOCALUSER
+		exit 0
+	fi
 	# test this against local users or usermap
 	# if true, return mapped username with $(echo "USERNAME")
 
+
+	# If LDAPSEARCH fails then fallback to local usermap file
 	for line in $(cat /etc/ood/bin/usermap); do
 		email=$(echo $line | awk -F, '{print $1}')
 		local_user=$(echo $line | awk -F, '{print $2}')
