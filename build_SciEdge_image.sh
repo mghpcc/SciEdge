231f3b1bbdaeccd4594cb1b01aea469a64cfec13
Morgan Ludwig <mjbludwig@gmail.com>
2023-09-12 13:55:10 -0400

    Update build_SciEdge_image.sh
    made buildah run into a variable for site specific tweaks

--- build_SciEdge_image.sh
+++ build_SciEdge_image.sh
@@ -4,22 +4,22 @@ GIT_ROOT=$HOME/SciEdge
 
 
 ctr=SciEdge_image
-
+buildah_run_command="sudo buildah run --net=host"
 
 sudo buildah from --name $ctr docker://rockylinux:8
-sudo buildah run $ctr -- dnf -y module enable ruby:2.7
-sudo buildah run $ctr -- dnf -y module enable nodejs:14
-sudo buildah run $ctr -- dnf install -y dnf-plugins-core systemd
-sudo buildah run $ctr -- dnf install -y epel-release
-sudo buildah run $ctr -- dnf config-manager --set-enabled powertools
-sudo buildah run $ctr -- yum install -y https://yum.osc.edu/ondemand/2.0/ondemand-release-web-2.0-1.noarch.rpm
-sudo buildah run $ctr -- dnf install -y ondemand vim openssh-server python3 findutils nc
-sudo buildah run $ctr -- dnf install -y mod_auth_openidc
-sudo buildah run $ctr -- dnf install -y mod_authnz_pam mod_proxy_html openldap-clients
+$buildah_run_command $ctr -- dnf -y module enable ruby:2.7
+$buildah_run_command $ctr -- dnf -y module enable nodejs:14
+$buildah_run_command $ctr -- dnf install -y dnf-plugins-core systemd
+$buildah_run_command $ctr -- dnf install -y epel-release
+$buildah_run_command $ctr -- dnf config-manager --set-enabled powertools
+$buildah_run_command $ctr -- yum install -y https://yum.osc.edu/ondemand/2.0/ondemand-release-web-2.0-1.noarch.rpm
+$builhda_run_command $ctr -- dnf install -y ondemand vim openssh-server python3 findutils nc
+$buildah_run_command $ctr -- dnf install -y mod_auth_openidc
+$builhda_run_command $ctr -- dnf install -y mod_authnz_pam mod_proxy_html openldap-clients
 sudo buildah run $ctr -- ln -s /usr/bin/python3 /usr/bin/python
-sudo buildah run $ctr -- python -m pip install virtualenv
-sudo buildah run $ctr /bin/bash -c 'curl https://turbovnc.org/pmwiki/uploads/Downloads/TurboVNC.repo > /etc/yum.repos.d/turbovnc.repo'
-sudo buildah run $ctr -- dnf install -y turbovnc
+$buildah_run_command $ctr -- python -m pip install virtualenv
+$buildah_run_command $ctr /bin/bash -c 'curl https://turbovnc.org/pmwiki/uploads/Downloads/TurboVNC.repo > /etc/yum.repos.d/turbovnc.repo'
+$buildah_run_command $ctr -- dnf install -y turbovnc
 sudo buildah run $ctr -- mv /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.off
 sudo buildah run $ctr -- mkdir /var/log/apache_testing
 # This section adds the ability to make system users that can login
