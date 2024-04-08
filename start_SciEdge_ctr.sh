894abdc91e1cabba2f942b5e975fd337bb24b8d5
Morgan Ludwig <mludwig@techsquare.com>
2024-04-08 16:18:02 -0400

    corrected podman run with the right updated flags

--- start_SciEdge_ctr.sh
+++ start_SciEdge_ctr.sh
@@ -6,5 +6,4 @@ SSL_CERT_ROOT_PATH=/etc/letsencrypt
 
 GIT_ROOT=$HOME/SciEdge
 
-sudo podman run -d --network=host --tz=America/New_York -v $GIT_ROOT/var/www/ood/apps/sys/Flask:/var/www/ood/apps/sys/Flask -v $GIT_ROOT/var/www/ood/apps/sys/dashboard/config/locales:/var/www/ood/apps/sys/dashboard/config/locales -v $SSL_CERT_ROOT_PATH:$SSL_CERT_ROOT_PATH -v $GIT_ROOT/etc/ood:/etc/ood -v $GIT_ROOT/etc/group:/etc/group -v $GIT_ROOT/etc/passwd:/etc/passwd -v /home:/home --userns host --name SciEdge_ctr SciEdge_image /usr/sbin/init
-
+podman run --privileged -d --network=host --tz=America/New_York -v $GIT_ROOT/var/www/ood/apps/sys/Flask:/var/www/ood/apps/sys/Flask -v $SSL_CERT_ROOT_PATH:$SSL_CERT_ROOT_PATH -v $GIT_ROOT/etc/ood:/etc/ood -v $GIT_ROOT/etc/group:/etc/group -v $GIT_ROOT/etc/passwd:/etc/passwd -v /home:/home --userns host --name sciedge_ctr sciedge_image /usr/sbin/init
